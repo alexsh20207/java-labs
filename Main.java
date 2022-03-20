6ec8eed5fbf4f23f1db10d73dfab562688732526
alexsh20207 <73633904+alexsh20207@users.noreply.github.com>
2022-03-20 20:02:25 +0700

    new_main

--- Main.java
+++ Main.java
@@ -1,40 +1,46 @@
-import lab1.Container;
-import lab1.Parser;
-import lab1.Reader;
-import lab1.Writer;
-
+package ru.nsu.oop.shtang.lab1;
 import java.io.IOException;
 
 public class Main {
     static private final int ARG_COUNT = 2;
     static private final String ARG_ERR = "Wrong args";
-    static private final int INPUT_ART = 0;
+    static private final int INPUT_ARG = 0;
     static private final int OUTPUT_ARG = 1;
     static private final int WORD_COUNT_INIT = 0;
     static private final String EMPTY_ERR = "Input is empty";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != ARG_COUNT) {
             System.out.print(ARG_ERR);
             return;
         }
+        Reader reader = null;
+        Writer writer = null;
+        Container container = null;
+        Parser parser = null;
         try {
-            Reader reader = new Reader(args[INPUT_ARG]);
-            Container container = new Container();
-            Parser parser = new Parser(reader, container, WORD_COUNT_INIT);
+            reader = new Reader(args[INPUT_ARG]);
+            container = new Container();
+            parser = new Parser(reader, container);
             parser.work();
             if (container.getWordCount() == WORD_COUNT_INIT) {
                 System.out.print(EMPTY_ERR);
                 return;
             }
-            Writer writer = new Writer(args[OUTPUT_ARG], container);
+            writer = new Writer(args[OUTPUT_ARG], container);
             writer.work();
-            writer.close();
-            reader.close();
         } catch (IOException err) {
             System.out.print(err.getMessage());
             err.printStackTrace();
             return;
+        } finally  {
+        if (reader != null) {
+            reader.close();
         }
+        if (writer != null) {
+            writer.close();
+        }
+    }
+
     }
 }
