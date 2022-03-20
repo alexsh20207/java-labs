bc0872ed5d0acca5eb03a32c9dd3ba3e48385395
alexsh20207 <73633904+alexsh20207@users.noreply.github.com>
2022-03-20 20:04:03 +0700

    Update Container.java

--- lab1/Container.java
+++ lab1/Container.java
@@ -1,22 +1,25 @@
-package lab1;
+package ru.nsu.oop.shtang.lab1;
+
+import javafx.util.Pair;
+
 import java.util.*;
 
 public class Container {
-    static private final int allWordsCountInit = 0;
-    static private final int wordCountInit = 1;
-    static private final int countStep = 1;
+    static private final int ALL_WORDS_COUNT_INIT = 0;
+    static private final int WORD_COUNT_INIT = 1;
+    static private final int COUNT_STEP = 1;
 
     private HashMap <String, Integer> words;
     private int wordCount;
 
     public Container() {
         words = new HashMap<String, Integer>();
-        wordCount = allWordsCountInit;
+        wordCount = ALL_WORDS_COUNT_INIT;
     }
     public void addWord(String word) {
-        if (words.containsKey(word)) words.put(word, words.get(word) + countStep);
+        if (words.containsKey(word)) words.put(word, words.get(word) + COUNT_STEP);
         else {
-            words.put(word, wordCountInit);
+            words.put(word, WORD_COUNT_INIT);
         }
         wordCount++;
     }
@@ -25,10 +28,19 @@ public class Container {
         return wordCount;
     }
 
-    public List<Map.Entry<String, Integer>> sort() {
-        List<Map.Entry<String, Integer>> list = new ArrayList<Map.Entry<String, Integer>>(words.entrySet());
-        list.sort(Map.Entry.comparingByValue());
+    public List<Pair<String, Integer>> sort() {
+        List<Pair<String, Integer>> list = new ArrayList<Pair<String, Integer>>();
+        for (String key: words.keySet()) {
+            list.add(new Pair<String, Integer>(key, words.get(key)));
+        }
+        list.sort(new Comparator<Pair<String, Integer>>() {
+            @Override
+            public int compare(final Pair<String, Integer> o1, final Pair<String, Integer> o2) {
+                return o1.getValue() - o2.getValue();
+            };
+        });
         return list;
+
     }
 
     public int getCount(String word) {
