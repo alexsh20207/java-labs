9fb0076a5723c0f6a98261acf3dfbf43144b627e
alexsh20207 <73633904+alexsh20207@users.noreply.github.com>
2022-03-20 20:02:58 +0700

    Update Writer.java

--- lab1/Writer.java
+++ lab1/Writer.java
@@ -1,15 +1,17 @@
-package lab1;
+package ru.nsu.oop.shtang.lab1;
+
+import javafx.util.Pair;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 public class Writer {
-    static private final int extraUnit = 1;
-    static private final int forInit = 0;
-    static private final int oneHundred = 100;
-
+    static private final int EXTRA_UNIT = 1;
+    static private final int FOR_LIMIT = 0;
+    static private final int ONE_HUNDRED = 100;
+    static private final char COMMA = ';';
+    static private final char NEW_LINE = '\n';
     private Container wordCont;
     private FileWriter fileWriter;
 
@@ -19,12 +21,12 @@ public class Writer {
     }
 
     public void work() throws IOException{
-        List<Map.Entry<String, Integer>> words = wordCont.sort();
+        List<Pair<String, Integer>> words = wordCont.sort();
         int wordCount = wordCont.getWordCount();
-        for (int i = words.size() - extraUnit; i >= forInit; i--) {
+        for (int i = words.size() - EXTRA_UNIT; i >= FOR_LIMIT; i--) {
             String thisWordCount = words.get(i).getValue().toString();
-            String freq = ((Double)(((double)words.get(i).getValue() / wordCount) * oneHundred)).toString();
-            fileWriter.write(words.get(i).getKey() + ";" + thisWordCount + ";" + freq + '\n');
+            String freq = ((Double)(((double)words.get(i).getValue() / wordCount) * ONE_HUNDRED)).toString();
+            fileWriter.write(words.get(i).getKey() + COMMA + thisWordCount + COMMA + freq + NEW_LINE);
         }
     }
     public void close() throws IOException {
