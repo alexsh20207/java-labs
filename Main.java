36f79e5b6cbef97dd0a2ccb5bdb7af20eab90d08
alexsh20207 <73633904+alexsh20207@users.noreply.github.com>
2022-05-16 09:45:28 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,10 @@
 package rus.nsu.fit.oop.ms;
 
-import java.awt.*;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        EventQueue.invokeLater(() -> {
-            Minesweeper minesweeper = null;
-            try {
-                minesweeper = new Minesweeper();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            minesweeper.setVisible(true);
-        });
+    public static void main(String[] args) throws IOException {
+        Minesweeper minesweeper = new Minesweeper();
+        minesweeper.setVisible(true);
     }
 }
